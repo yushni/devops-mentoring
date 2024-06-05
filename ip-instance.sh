5d433f57ddca6c4d24d47ddba12e17db9a0f5956
yshnitsar <120453390+yshnitsar@users.noreply.github.com>
2024-06-05 10:11:57 +0300

    init

---
+++ ip-instance.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function verifyInstanceHasOutboundConnection() {
+  for i in {1..10}; do
+    resp=$(curl -m 5 -I http://google.com)
+    if [ "$resp" ]; then
+      echo "Instance has outbound connection"
+      return
+    fi
+    echo "Instance does not have outbound connection retrying in 5 seconds"
+    sleep 5
+  done
+}
+
+function installNginx() {
+  echo "Trying yum install"
+  yum update -y
+  yum install nginx -y
+  echo "Nginx installed"
+}
+
+function retrieveAndWriteTokenToNginxIndexFile() {
+  echo "Retrieving token from metadata service"
+  TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+  echo "Retrieving local IP address from metadata service"
+  LOCALIP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
+
+  echo "Writing local IP address to index.html"
+  echo $LOCALIP >/usr/share/nginx/html/index.html
+}
+
+function startNginx() {
+  echo "Starting Nginx"
+  systemctl start nginx
+}
+
+function main() {
+  verifyInstanceHasOutboundConnection
+  installNginx
+  retrieveAndWriteTokenToNginxIndexFile
+  startNginx
+}
+
+main
